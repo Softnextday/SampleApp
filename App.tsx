1712926c24b70516b1ef480be25b1956b47e8908
ksd711 <govindcarpenter23@gmail.com>
2024-07-25 19:01:23 +0530

    Resolved merge conflicts

--- App.tsx
+++ App.tsx
@@ -3,25 +3,14 @@ import React from 'react'
 
 export default function App() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+<<<<<<< HEAD
+    <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'lightgreen'}}>
+      <Text style={{fontSize:22,color:'#3300ff',fontWeight:'700'}}>Sample App</Text>
+      <Text style={{fontSize:22,color:'#3300ff',fontWeight:'700'}}>push</Text>
+=======
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text style={{fontSize:22,color:'#3300ff'}}>Sample App</Text>
+>>>>>>> 1fd487867c08cea03c39368779c7b67706119c45
     </View>
   )
 }
