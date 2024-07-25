9f2b6152d9b4e07e9919181ce7644fd64864d1d5
ksd711 <govindcarpenter23@gmail.com>
2024-07-25 18:56:03 +0530

    text

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'lightgreen'}}>
       <Text style={{fontSize:22,color:'#3300ff',fontWeight:'700'}}>Sample App</Text>
+      <Text style={{fontSize:22,color:'#3300ff',fontWeight:'700'}}>push</Text>
     </View>
   )
 }
